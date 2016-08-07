8fe937fc87dfc6a95f594961a07281c45a971a99
Toaster <jonathan.pereira@gmail.com>
2016-08-07 02:06:12 -0300

    Diminuição das bases

--- config/base.hpp
+++ config/base.hpp
@@ -1,18 +1,18 @@
-class aeroportdetanoa
-{
-	name = "Aéroport de Tanoa";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class aeroportdetanoa
+// {
+// 	name = "Aéroport de Tanoa";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
 class aegis
 {
@@ -46,85 +46,85 @@ class balaairstrip
 	vehicles[] = {};
 };
 
-class camp
-{
-	name = "Camp";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class camp
+// {
+// 	name = "Camp";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class commsalpha
-{
-	name = "Comms Alpha";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class commsalpha
+// {
+// 	name = "Comms Alpha";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class commsbravo
-{
-	name = "Comms Bravo";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class commsbravo
+// {
+// 	name = "Comms Bravo";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class fueldepot
-{
-	name = "Fuel Depot";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class fueldepot
+// {
+// 	name = "Fuel Depot";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class gsmstation
-{
-	name = "GSM Station";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class gsmstation
+// {
+// 	name = "GSM Station";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
 class hotelresort
 {
@@ -142,69 +142,69 @@ class hotelresort
 	vehicles[] = {};
 };
 
-class rochelleaerodrome
-{
-	name = "Rochelle Aerodrome";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class rochelleaerodrome
+// {
+// 	name = "Rochelle Aerodrome";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class lumberyard
-{
-	name = "Lumberyard";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class lumberyard
+// {
+// 	name = "Lumberyard";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class plantation
-{
-	name = "Plantation";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class plantation
+// {
+// 	name = "Plantation";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
-class redsprings
-{
-	name = "Redsprings";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class redsprings
+// {
+// 	name = "Redsprings";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
 class saintgeorgeairstrip
 {
@@ -238,19 +238,19 @@ class tanoasugarcompany
 	vehicles[] = {};
 };
 
-class tuvanakaairbase
-{
-	name = "Tuvanaka Airbase";
-	isOwned = 0;
-	groups = 2;
-	respawnGroups = 1;
-	respawnTime = 300;
-	// vehicles[] =
-	// {
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
-	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
-	// };
-	vehicles[] = {};
-};
+// class tuvanakaairbase
+// {
+// 	name = "Tuvanaka Airbase";
+// 	isOwned = 0;
+// 	groups = 2;
+// 	respawnGroups = 1;
+// 	respawnTime = 300;
+// 	// vehicles[] =
+// 	// {
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_1"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_2"},
+// 	// 	{"B_Truck_01_transport_F", "base_camp_respawn_3"}
+// 	// };
+// 	vehicles[] = {};
+// };
 
